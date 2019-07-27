440e0da85d272b45ae34b6bfb971127c8b88ee93
Austin Caron <aus10caron@icloud.com>
2019-07-27 11:39:42 -0400

    Dynamic adding of new buttons + localStorage

--- browser/browser.js
+++ browser/browser.js
@@ -4,22 +4,72 @@ const createButton = document.getElementById('create');
 const nameInput = document.getElementById('btn-name');
 const actionInput = document.getElementById('btn-action');
 const colorPicker = document.getElementById('color-picker');
+const existingContainer = document.getElementById('existing');
 
+// return the local storage array
+const returnLocalStorage = () => {
+    const list = localStorage.getItem("buttons");
+    const parsedList = JSON.parse(list);
+
+    // check if the list contains anything
+    if(parsedList === null) {
+        return []
+    } else {
+        return parsedList;
+    }
+}
+
+// fill the existing container
+function setList() {
+    const currentList = returnLocalStorage();
+
+    console.log(currentList)
+    // if empty then skip setting the list
+    if(currentList === []) return;
+
+    // run a loop through the arry
+    currentList.forEach(button => {
+        console.log("Button: ", button)
+        let circle = `<div class="circle" style="background: ${button.color}"></div>`;
+        let title = `<p>${button.name}</p>`;
+
+        let buttonItem = `<div class="button-item active">${circle} ${title}</div>`;
+        existingContainer.innerHTML += buttonItem;
+    });
+
+    ipcRenderer.send('create-new-button-array', (e, currentList));
+}
+
+// when the page first loads, set the list
+window.onload = setList;
+
+// listen for a click to create a new button
 createButton.addEventListener('click', (e) => {
     e.preventDefault();
-    console.log('clicked')
+
+    // create a new object
     const newTouchButton = {
         name: nameInput.value,
         action: actionInput.value,
         color: colorPicker.value
     };
 
-    const buttons = [newTouchButton];
+    // store it in an array combined with what's currently stored
+    const buttons = [ ...returnLocalStorage(), newTouchButton ];
 
+    // add it to an array
     localStorage.setItem("buttons", JSON.stringify(buttons));
 
-    ipcRenderer.send('create-new-button', (e, newTouchButton));
+    // send an event to the main
+    // ipcRenderer.send('create-new-button', (e, newTouchButton));
 
+    // reset values
     nameInput.value = '';
     actionInput.value = '';
+
+    // remove existing dom elements
+    while (existingContainer.firstChild) existingContainer.removeChild(existingContainer.firstChild);
+
+    // refresh existing container
+    setList();
 });
