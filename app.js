8f64923fe95769f7162f600585962fc55ea9ae5f
Austin Caron <aus10caron@icloud.com>
2019-07-28 14:24:13 -0400

    finally fixed the formatting glitch

--- app.js
+++ app.js
@@ -1,70 +1,79 @@
-const { app, BrowserWindow, TouchBar, ipcMain, shell, Menu } = require('electron')
-const applicationInfo = require('./package.json')
+const {
+  app,
+  BrowserWindow,
+  TouchBar,
+  ipcMain,
+  shell,
+  Menu
+} = require("electron");
+const applicationInfo = require("./package.json");
 
-const { TouchBarLabel, TouchBarButton, TouchBarSpacer } = TouchBar
+const { TouchBarLabel, TouchBarButton, TouchBarSpacer } = TouchBar;
 
-const smallSpacer = new TouchBarSpacer({ size: 'small' })
-const largeSpacer = new TouchBarSpacer({ size: 'large' })
+const smallSpacer = new TouchBarSpacer({ size: "small" });
+const largeSpacer = new TouchBarSpacer({ size: "large" });
 
 function createTheTouchbar(listOfButtons) {
-    // map out each button
+  // map out each button
 
-    const filteredButtons = listOfButtons.filter((item) => item.type === "button")
-    const filteredLabel = listOfButtons.filter((item) => item.type === "label")
+  const filteredButtons = listOfButtons.filter(item => item.type === "button");
+  const filteredLabel = listOfButtons.filter(item => item.type === "label");
 
-    const touchBarButtons = filteredButtons.map(({name, color, action, type}) => {
-        return new TouchBarButton({
-            label: name,
-            backgroundColor: color,
-                click: () => {
-                // Use the shell to open links in the user browser
-                shell.openExternal(action)
-            }
-        });
-    });
+  const touchBarButtons = filteredButtons.map(
+    ({ name, color, action, type }) => {
+      return new TouchBarButton({
+        label: name,
+        backgroundColor: color,
+        click: () => {
+          // Use the shell to open links in the user browser
+          shell.openExternal(action);
+        }
+      });
+    }
+  );
 
-    // map out welcome label
-    const touchBarWelcomeLabel = filteredLabel.map(({name, type}) => {
-        return new TouchBarLabel({label: `Welcome ${name}`})
-    })
-    
-    const items = [smallSpacer, ...touchBarButtons]
-    const escapeItem = touchBarWelcomeLabel[0]
+  // map out welcome label
+  const touchBarWelcomeLabel = filteredLabel.map(({ name, type }) => {
+    return new TouchBarLabel({ label: `Welcome ${name}` });
+  });
 
-    // create the touchbar
-    const touchBar = new TouchBar({ items, escapeItem });
-    
-	// grab the current window
-    const [win] = BrowserWindow.getAllWindows();
+  const items = [smallSpacer, ...touchBarButtons];
+  const escapeItem = touchBarWelcomeLabel[0];
 
-    // create the touchbar
-	win.setTouchBar(touchBar);
+  // create the touchbar
+  const touchBar = new TouchBar({ items, escapeItem });
+
+  // grab the current window
+  const [win] = BrowserWindow.getAllWindows();
+
+  // create the touchbar
+  win.setTouchBar(touchBar);
 }
 
 function createWindow() {
-    let win = new BrowserWindow({
-        width: 700,
-        height: 485,
-        darkTheme: true,
-        titleBarStyle: 'hiddenInset',
-        webPreferences: {
-            nodeIntegration: true,
-        },
-    })
+  let win = new BrowserWindow({
+    width: 700,
+    height: 485,
+    darkTheme: true,
+    titleBarStyle: "hiddenInset",
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
 
-    win.loadFile('browser/index.html')
+  win.loadFile("browser/index.html");
 }
 
 app.setAboutPanelOptions({
-    applicationName: "Short Touch",
-    applicationVersion: applicationInfo.version,
-    version: "beta",
-    copyright: "Copyright © 2019 Sunstrous"
-})
+  applicationName: "Short Touch",
+  applicationVersion: applicationInfo.version,
+  version: "beta",
+  copyright: "Copyright © 2019 Sunstrous"
+});
 
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
 // catch event from browser.js
-ipcMain.on('create-new-button-array', (e, list) => {
-    createTheTouchbar(list)
-})
+ipcMain.on("create-new-button-array", (e, list) => {
+  createTheTouchbar(list);
+});
--- browser/browser.js
+++ browser/browser.js
@@ -1,311 +1,315 @@
-const { ipcRenderer, shell } = require('electron');
+const { ipcRenderer, shell } = require("electron");
 
 // form buttons and inputs
-const createButton = document.getElementById('create');
-const updateButton = document.getElementById('update');
-const deleteButton = document.getElementById('delete');
-const updateLabelButton = document.getElementById('update-label');
-const nameInput = document.getElementById('btn-name');
-const actionInput = document.getElementById('btn-action');
-const colorPicker = document.getElementById('color-picker');
-const nameEditInput = document.getElementById('btn-name-edit');
-const actionEditInput = document.getElementById('btn-action-edit');
-const colorPickerEdit = document.getElementById('color-picker-edit');
-const labelNameInput = document.getElementById('btn-name-edit-label');
+const createButton = document.getElementById("create");
+const updateButton = document.getElementById("update");
+const deleteButton = document.getElementById("delete");
+const updateLabelButton = document.getElementById("update-label");
+const nameInput = document.getElementById("btn-name");
+const actionInput = document.getElementById("btn-action");
+const colorPicker = document.getElementById("color-picker");
+const nameEditInput = document.getElementById("btn-name-edit");
+const actionEditInput = document.getElementById("btn-action-edit");
+const colorPickerEdit = document.getElementById("color-picker-edit");
+const labelNameInput = document.getElementById("btn-name-edit-label");
 
 // existing container
-const existingContainer = document.getElementById('existing');
-const labelContainer = document.getElementById('label-container');
-const buttonsContainer = document.getElementById('buttons-container');
-const existingButtons = document.getElementsByClassName('button-item');
+const existingContainer = document.getElementById("existing");
+const labelContainer = document.getElementById("label-container");
+const buttonsContainer = document.getElementById("buttons-container");
+const existingButtons = document.getElementsByClassName("button-item");
 
 // tabs
-const createTab = document.getElementById('tab-create');
-const editTab = document.getElementById('tab-edit');
+const createTab = document.getElementById("tab-create");
+const editTab = document.getElementById("tab-edit");
 
 // form pages
-const createForm = document.getElementById('create-form');
-const editForm = document.getElementById('edit-form');
-const labelForm = document.getElementById('label-form');
+const createForm = document.getElementById("create-form");
+const editForm = document.getElementById("edit-form");
+const labelForm = document.getElementById("label-form");
 
 // return the local storage array
 const returnLocalStorage = () => {
-    const list = localStorage.getItem("buttons");
-    const parsedList = JSON.parse(list);
+  const list = localStorage.getItem("buttons");
+  const parsedList = JSON.parse(list);
 
-    // check if the list contains anything
-    if(parsedList === null) {
-        return []
-    } else {
-        return parsedList;
-    }
-}
+  // check if the list contains anything
+  if (parsedList === null) {
+    return [];
+  } else {
+    return parsedList;
+  }
+};
 
 // fill the existing container
 function setList() {
-    const currentList = returnLocalStorage();
+  const currentList = returnLocalStorage();
 
-    // if empty then skip setting the list
-    if(currentList === []) return;
+  // if empty then skip setting the list
+  if (currentList === []) return;
 
-    // run a loop through the array
-    currentList.forEach(button => {
-        let {name, color, action, type} = button;
+  // run a loop through the array
+  currentList.forEach(button => {
+    let { name, color, action, type } = button;
 
-        if(type == "button") {
-            let circle = `<div class="circle" style="background: ${color}"></div>`;
-            let title = `<p>${name}</p>`;
-    
-            let buttonItem = `<div name="${name}" action="${action}" color="${color}" class="button-item">${circle} ${title}</div>`;
-            buttonsContainer.innerHTML += buttonItem;
-        } else if (type == "label") {
-            let circle = `<div class="circle" style="background: ${color}"></div>`;
-            let title = `<p>${name}</p>`;
-    
-            let buttonItem = `<div type="label" name="${name}" action="//" color="#353535" class="button-item name-label">${circle} ${title}</div>`;
-            labelContainer.innerHTML += buttonItem;
-        }
-    });
+    if (type == "button") {
+      let circle = `<div class="circle" style="background: ${color}"></div>`;
+      let title = `<p>${name}</p>`;
 
-    ipcRenderer.send('create-new-button-array', (currentList));
+      let buttonItem = `<div name="${name}" action="${action}" color="${color}" class="button-item">${circle} ${title}</div>`;
+      buttonsContainer.innerHTML += buttonItem;
+    } else if (type == "label") {
+      let circle = `<div class="circle" style="background: ${color}"></div>`;
+      let title = `<p>${name}</p>`;
 
-    // for loop that will add event listeners to each div
-    for (let i = 0; i < existingButtons.length; i++) {
-        existingButtons[i].addEventListener('click', (e) => {
-            // define the element
-            let element = e.target
-
-            // make sure we can't click on inner elements
-            if(element.className == "button-item name-label") {
-                removeActiveClass()
-                element.classList.add('active');
-                switchToLabelPage(element)
-            }
-
-            // make sure we can't click on inner elements
-            if(element.className == "button-item") {
-                removeActiveClass()
-                element.classList.add('active');
-                switchToEditPage(element)
-            }
-        });
+      let buttonItem = `<div type="label" name="${name}" action="//" color="#353535" class="button-item name-label">${circle} ${title}</div>`;
+      labelContainer.innerHTML += buttonItem;
     }
+  });
+
+  ipcRenderer.send("create-new-button-array", currentList);
+
+  // for loop that will add event listeners to each div
+  for (let i = 0; i < existingButtons.length; i++) {
+    existingButtons[i].addEventListener("click", e => {
+      // define the element
+      let element = e.target;
+
+      // make sure we can't click on inner elements
+      if (element.className == "button-item name-label") {
+        removeActiveClass();
+        element.classList.add("active");
+        switchToLabelPage(element);
+      }
+
+      // make sure we can't click on inner elements
+      if (element.className == "button-item") {
+        removeActiveClass();
+        element.classList.add("active");
+        switchToEditPage(element);
+      }
+    });
+  }
 }
 
 // remove an existing active class when switching elements
 function removeActiveClass() {
-    for (let i = 0; i < existingButtons.length; i++) {
-        let button = existingButtons[i]
+  for (let i = 0; i < existingButtons.length; i++) {
+    let button = existingButtons[i];
 
-        // check to see if it has the active class
-        if(button.classList[1] == 'active') {
-            button.classList.remove('active')
-        } else if (button.classList[2] == 'active') {
-            button.classList.remove('active')
-        }
+    // check to see if it has the active class
+    if (button.classList[1] == "active") {
+      button.classList.remove("active");
+    } else if (button.classList[2] == "active") {
+      button.classList.remove("active");
     }
+  }
 }
 
 function switchToEditPage(button) {
-    editTab.classList.add('active')
-    createTab.classList.remove('active')
-    labelForm.classList.add('hidden')
-    createForm.classList.add('hidden')
-    editForm.classList.remove('hidden')
+  editTab.classList.add("active");
+  createTab.classList.remove("active");
+  labelForm.classList.add("hidden");
+  createForm.classList.add("hidden");
+  editForm.classList.remove("hidden");
 
-    // load the values into the inputs
-    nameEditInput.value = button.getAttribute('name')
-    actionEditInput.value = button.getAttribute('action')
-    colorPickerEdit.value = button.getAttribute('color')
+  // load the values into the inputs
+  nameEditInput.value = button.getAttribute("name");
+  actionEditInput.value = button.getAttribute("action");
+  colorPickerEdit.value = button.getAttribute("color");
 }
 
 function switchToLabelPage(button) {
-    editTab.classList.add('active')
-    createTab.classList.remove('active')
-    createForm.classList.add('hidden')
-    editForm.classList.add('hidden')
-    labelForm.classList.remove('hidden')
+  editTab.classList.add("active");
+  createTab.classList.remove("active");
+  createForm.classList.add("hidden");
+  editForm.classList.add("hidden");
+  labelForm.classList.remove("hidden");
 
-    // load the values into the inputs
-    labelNameInput.value = button.getAttribute('name')
+  // load the values into the inputs
+  labelNameInput.value = button.getAttribute("name");
 }
 
 function switchToCreatePage() {
-    editTab.classList.remove('active')
-    createTab.classList.add('active')
-    createForm.classList.remove('hidden')
-    editForm.classList.add('hidden')
-    labelForm.classList.add('hidden')
-    removeActiveClass()
+  editTab.classList.remove("active");
+  createTab.classList.add("active");
+  createForm.classList.remove("hidden");
+  editForm.classList.add("hidden");
+  labelForm.classList.add("hidden");
+  removeActiveClass();
 }
 
 function isStorageFull() {
-    const currentList = returnLocalStorage();
+  const currentList = returnLocalStorage();
 
-    // touch bar length can't be more than 6
-    if(currentList.length < 7 ) {
-        return true
-    } else {
-        return false
-    }
+  // touch bar length can't be more than 6
+  if (currentList.length < 7) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function cleanExistingContainer() {
-    // remove existing dom elements
-    while (labelContainer.firstChild) labelContainer.removeChild(labelContainer.firstChild);
-    while (buttonsContainer.firstChild) buttonsContainer.removeChild(buttonsContainer.firstChild);
+  // remove existing dom elements
+  while (labelContainer.firstChild)
+    labelContainer.removeChild(labelContainer.firstChild);
+  while (buttonsContainer.firstChild)
+    buttonsContainer.removeChild(buttonsContainer.firstChild);
 
-    const userText = `<p class="container-text">user</p>`
-    const buttonText = `<p class="container-text">button</p>`
-    labelContainer.innerHTML += userText
-    buttonsContainer.innerHTML += buttonText
+  const userText = `<p class="container-text">user</p>`;
+  const buttonText = `<p class="container-text">button</p>`;
+  labelContainer.innerHTML += userText;
+  buttonsContainer.innerHTML += buttonText;
 }
 
 // when the page first loads, set the list
 window.onload = setList;
 
 // listen for a click to create a new button
-createButton.addEventListener('click', (e) => {
-    e.preventDefault();
+createButton.addEventListener("click", e => {
+  e.preventDefault();
 
-    const notFull = isStorageFull()
+  const notFull = isStorageFull();
 
-    if(!notFull) {
-        shell.beep()
-        return alert("Can't have more than 6 buttons!")
-    }
+  if (!notFull) {
+    shell.beep();
+    return alert("Can't have more than 6 buttons!");
+  }
 
-    // create a new object
-    const newTouchButton = {
-        name: nameInput.value,
-        action: actionInput.value,
-        color: colorPicker.value,
-        type: "button"
-    };
+  // create a new object
+  const newTouchButton = {
+    name: nameInput.value,
+    action: actionInput.value,
+    color: colorPicker.value,
+    type: "button"
+  };
 
-    // store it in an array combined with what's currently stored
-    const buttons = [ ...returnLocalStorage(), newTouchButton ];
+  // store it in an array combined with what's currently stored
+  const buttons = [...returnLocalStorage(), newTouchButton];
 
-    // add it to an array
-    localStorage.setItem("buttons", JSON.stringify(buttons));
+  // add it to an array
+  localStorage.setItem("buttons", JSON.stringify(buttons));
 
-    // reset values
-    nameInput.value = '';
-    actionInput.value = '';
-    colorPicker.value = '#f4f4f4';
+  // reset values
+  nameInput.value = "";
+  actionInput.value = "";
+  colorPicker.value = "#f4f4f4";
 
-    cleanExistingContainer()
+  cleanExistingContainer();
 
-    // refresh existing container
-    setList();
+  // refresh existing container
+  setList();
 });
 
 // switch to create tab
-createTab.addEventListener('click', switchToCreatePage);
+createTab.addEventListener("click", switchToCreatePage);
 
 // switch to edit tab
-editTab.addEventListener('click', () => {
-    const element = existingButtons[1];
-    if(element) {
-        switchToEditPage(element);
-        element.classList.add('active');
-    } else {
-        shell.beep();
-    }
-})
+editTab.addEventListener("click", () => {
+  const element = existingButtons[1];
+  if (element) {
+    switchToEditPage(element);
+    element.classList.add("active");
+  } else {
+    shell.beep();
+  }
+});
 
 // listen for a click on the update button
-updateButton.addEventListener('click', (e) => {
-    e.preventDefault();
+updateButton.addEventListener("click", e => {
+  e.preventDefault();
 
-    const currentList = returnLocalStorage();
+  const currentList = returnLocalStorage();
 
-    // create a new object
-    const updateTouchButton = {
-        name: nameEditInput.value,
-        action: actionEditInput.value,
-        color: colorPickerEdit.value,
-        type: "button"
-    };
+  // create a new object
+  const updateTouchButton = {
+    name: nameEditInput.value,
+    action: actionEditInput.value,
+    color: colorPickerEdit.value,
+    type: "button"
+  };
 
-    // update the list with the edited values
-    const updatedList = currentList.map((button) => {
-        if(button.name == nameEditInput.value) {
-            return updateTouchButton
-        } else {
-            return button
-        }
-    })
+  // update the list with the edited values
+  const updatedList = currentList.map(button => {
+    if (button.name == nameEditInput.value) {
+      return updateTouchButton;
+    } else {
+      return button;
+    }
+  });
 
-    // update the localStorage
-    localStorage.setItem("buttons", JSON.stringify(updatedList));
+  // update the localStorage
+  localStorage.setItem("buttons", JSON.stringify(updatedList));
 
-    // reset values
-    nameEditInput.value = '';
-    actionEditInput.value = '';
-    colorPickerEdit.value = '#f4f4f4';
+  // reset values
+  nameEditInput.value = "";
+  actionEditInput.value = "";
+  colorPickerEdit.value = "#f4f4f4";
 
-    cleanExistingContainer()
+  cleanExistingContainer();
 
-    // refresh existing container and switch to create;
-    setList();
-    switchToCreatePage();
-})
+  // refresh existing container and switch to create;
+  setList();
+  switchToCreatePage();
+});
 
 // listen for a click on the update-label button
-updateLabelButton.addEventListener('click', (e) => {
-    e.preventDefault();
+updateLabelButton.addEventListener("click", e => {
+  e.preventDefault();
 
-    const currentList = returnLocalStorage();
+  const currentList = returnLocalStorage();
 
-    // create a new object
-    const updateNameLabel = {
-        name: labelNameInput.value,
-        type: "label"
-    };
+  // create a new object
+  const updateNameLabel = {
+    name: labelNameInput.value,
+    type: "label"
+  };
 
-    // update the list with the edited values
-    const updatedList = currentList.map((button) => {
-        if(button.type == "label") {
-            return updateNameLabel
-        } else {
-            return button
-        }
-    })
+  // update the list with the edited values
+  const updatedList = currentList.map(button => {
+    if (button.type == "label") {
+      return updateNameLabel;
+    } else {
+      return button;
+    }
+  });
 
-    // update the localStorage
-    localStorage.setItem("buttons", JSON.stringify(updatedList));
+  // update the localStorage
+  localStorage.setItem("buttons", JSON.stringify(updatedList));
 
-    // reset values
-    labelNameInput.value = '';
+  // reset values
+  labelNameInput.value = "";
 
-    cleanExistingContainer()
+  cleanExistingContainer();
 
-    // refresh existing container and switch to create;
-    setList();
-    switchToCreatePage();
-})
+  // refresh existing container and switch to create;
+  setList();
+  switchToCreatePage();
+});
 
 // listen for a click on the delete button
-deleteButton.addEventListener('click', (e) => {
-    e.preventDefault()
+deleteButton.addEventListener("click", e => {
+  e.preventDefault();
 
-    const currentList = returnLocalStorage();
+  const currentList = returnLocalStorage();
 
-    // update the list with the edited values
-    const updatedList = currentList.filter((button) => button.name != nameEditInput.value)
+  // update the list with the edited values
+  const updatedList = currentList.filter(
+    button => button.name != nameEditInput.value
+  );
 
-    // update the localStorage
-    localStorage.setItem("buttons", JSON.stringify(updatedList));
+  // update the localStorage
+  localStorage.setItem("buttons", JSON.stringify(updatedList));
 
-    // reset values
-    nameEditInput.value = '';
-    actionEditInput.value = '';
-    colorPickerEdit.value = '#f4f4f4';
+  // reset values
+  nameEditInput.value = "";
+  actionEditInput.value = "";
+  colorPickerEdit.value = "#f4f4f4";
 
-    cleanExistingContainer()
+  cleanExistingContainer();
 
-    // refresh existing container and switch to create;
-    setList();
-    switchToCreatePage();
-})
+  // refresh existing container and switch to create;
+  setList();
+  switchToCreatePage();
+});
